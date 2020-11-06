06169f669a7e7270d128dc9efcdfde8839c25873
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-06 20:30:02 -0300

    Criação do model User

---
+++ backend/src/models/User.ts
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+interface IUser extends mongoose.Document {
+    name: string;
+    email: string;
+    password: string;
+}
+
+const UserSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        select: false,
+    },
+});
+
+UserSchema.pre('save', async function (this: IUser, next) {
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+
+    next();
+});
+
+export default mongoose.model<IUser>('User', UserSchema);
