dea2bbf31f1e5bcfd5dce66b2e922c6af1e7cece
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-06 21:02:36 -0200

    Configurando banco (MongoDB) com mongoose

---
+++ backend/src/database/connections.ts
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose';
+
+mongoose.connect(
+    'mongodb+srv://squad2:squad2@cluster0.iy1dd.mongodb.net/fifo',
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    }
+);
