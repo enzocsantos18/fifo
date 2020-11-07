0f1d42f12946f42c31107d4dff3fa63b5fa97bfc
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-07 18:20:22 -0200

    Rota de criaçao de agendamento iniciada

---
+++ backend/src/controllers/ScheduleController.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from 'express';
+import Schedule from '../models/Schedule';
+
+class ScheduleController {
+    public async index (req: Request, res: Response): Promise<Response> {
+            return;
+    }
+    public async create (req: Request, res: Response): Promise<Response> {
+        const { date } = req.body;
+
+        try {
+            if( await Schedule.findOne({ date }))
+            return res.status(400).send({ error: 'Schedule already exists'});
+
+            const createSchedule = await Schedule.create(req.body);
+
+            return res.send(createSchedule);
+        } catch (err) {
+            return res.status(400).send({ error: 'Schedule failed'});
+        }
+    }
+    public async update (req: Request, res: Response): Promise<Response> {
+        return;
+    }
+    public async delete (req: Request, res: Response): Promise<Response> {
+        return;
+    }
+}
+
+export default new ScheduleController;
