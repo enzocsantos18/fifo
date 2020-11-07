5fc649e8f7c29edf3a8b7c0b2dfed5bab64b2956
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-07 11:13:28 -0200

    Importaçao das rotas no arquivo app.ts, router.ts

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,8 +1,12 @@
 import express from 'express';
 import cors from 'cors';
 
+import Router from './router';
+
 const app = express();
+
 app.use(cors());
 app.use(express.json());
+app.use(Router);
 
 app.listen(3333);
