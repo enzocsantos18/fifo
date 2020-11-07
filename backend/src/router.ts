b89df3b00ae5e42e2ace862e2f008b0843d91fdc
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-07 18:29:09 -0300

    Rotas atualizadas

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -1,5 +1,18 @@
 import { Router } from 'express';
+import GameController from './controllers/GameController';
+import UserController from './controllers/UserController';
+import ScheduleController from './controllers/ScheduleController';
 
 const router = Router();
+
+router.post('/users', UserController.create);
+
+router.get('/games', GameController.index);
+router.post('/games', GameController.create);
+
+router.get('/schedules', ScheduleController.index);
+router.post('/schedules', ScheduleController.create);
+router.patch('/schedules', ScheduleController.update);
+router.delete('/schedules', ScheduleController.delete);
 
 export default router;
