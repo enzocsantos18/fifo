1fe1fade6f95ba2662722ae80f420261d4194ddc
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-08 01:34:47 -0300

    Rotas de autenticação adicionadas

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -2,6 +2,9 @@ import { Router } from 'express';
 import GameController from './controllers/GameController';
 import UserController from './controllers/UserController';
 import ScheduleController from './controllers/ScheduleController';
+import AuthController from './controllers/AuthController';
+
+import AuthMiddleware from './middlewares/auth';
 
 const router = Router();
 
@@ -12,7 +15,10 @@ router.post('/games', GameController.create);
 
 router.get('/schedules', ScheduleController.index);
 router.post('/schedules', ScheduleController.create);
-router.patch('/schedules', ScheduleController.update);
-router.delete('/schedules', ScheduleController.delete);
+router.patch('/schedules/:id', ScheduleController.update);
+router.delete('/schedules/:id', ScheduleController.delete);
+
+router.post('/auth/login', AuthController.login);
+router.post('/auth/logout', AuthController.logout);
 
 export default router;
