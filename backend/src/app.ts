c7d8bb0ff83eba4f55190a3de86d711d792a2c8a
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-08 00:34:17 -0300

    Helmet configurado

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,12 +1,13 @@
 import express from 'express';
 import cors from 'cors';
-
+import helmet from 'helmet';
 import router from './router';
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(helmet());
 app.use(router);
 
 app.listen(3333);
