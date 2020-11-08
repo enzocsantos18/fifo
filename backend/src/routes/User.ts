7c8ce294e324140f2e9aec0e361444b14473b352
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-07 23:59:45 -0300

    Merge para a dev e pasta de rotas removida

--- backend/src/routes/User.ts
+++
@@ -1,5 +0,0 @@
-import Router from '../router';
-import UserController from '../controllers/UserController';
-
-Router.get('/users', UserController.index);
-Router.post('/users', UserController.create);
