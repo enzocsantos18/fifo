dc3a6ffd2b5ac5ae186e905bdc7bc3a012e868f0
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-14 16:19:27 -0300

    Página de agendamento criada

---
+++ frontend/src/pages/Schedule/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import { MdArrowBack, MdArrowForward } from 'react-icons/md';
+import DayPicker from '../../components/DayPicker';
+import Button from '../../components/Input/Button';
+import { RangeInput, RangeLabels } from '../../components/Input/Range/indes';
+import TimePicker from '../../components/TimePicker';
+import { Container, Wrapper, Actions } from './styles';
+
+const Schedule: React.FC = () => {
+    return (
+        <Wrapper>
+            <Container>
+                <h1>Agendar</h1>
+
+                <label>Dia</label>
+                <DayPicker />
+
+                <label>Horário</label>
+                <TimePicker />
+                <label>Tempo</label>
+                <RangeInput max={60} min={15} step={15} defaultValue={15} />
+                <RangeLabels>
+                    <span>15 min.</span>
+                    <span>30 min.</span>
+                    <span>45 min.</span>
+                    <span>60 min.</span>
+                </RangeLabels>
+                <Actions>
+                    <Button variant='secondary'>
+                        <MdArrowBack />
+                        Voltar
+                    </Button>
+                    <Button variant='primary'>
+                        Prosseguir
+                        <MdArrowForward />
+                    </Button>
+                </Actions>
+            </Container>
+        </Wrapper>
+    );
+};
+
+export default Schedule;
---
+++ frontend/src/pages/Schedule/styles.ts
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+    width: 100vw;
+    height: 100vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+`;
+
+export const Container = styled.div`
+    width: 900px;
+    background: #fff;
+    border-radius: 8px;
+    padding: 30px 40px;
+`;
+
+export const Actions = styled.div`
+    display: flex;
+    justify-content: space-between;
+    margin-top: 30px;
+
+    & button {
+        width: 48%;
+    }
+`;
