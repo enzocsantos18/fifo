c2ae25f3eeaf563caf15db01c4efd49ffd6d0103
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-14 17:53:34 -0300

    Adicionado paginação e busca de jogos

--- frontend/src/pages/SelectGame/index.tsx
+++ frontend/src/pages/SelectGame/index.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import { MdSearch } from 'react-icons/md';
 import TextInput from '../../components/Input/Text';
 import { Wrapper, Container, GameList, GameBanner } from './styles';
+import { useHistory } from 'react-router-dom';
+import { debounce } from 'lodash';
+import { CircleSpinner } from 'react-spinners-kit';
 
-const games = [
+interface IGame {
+    name: string;
+    imageUrl: string;
+}
+
+const staticGames = [
     {
         name: 'FIFA 20',
         imageUrl:
@@ -17,17 +25,62 @@ const games = [
 ];
 
 const SelectGame: React.FC = () => {
+    const [searchQuery, setSearchQuery] = useState('');
+    const [isSearching, setSearching] = useState(false);
+    const [games, setGames] = useState<IGame[]>([]);
+    const history = useHistory();
+
+    function handleClick(name: string) {
+        history.push('/schedule/station', {
+            name,
+        });
+    }
+
+    function search() {
+        setSearching(false);
+        if (searchQuery === '') {
+            setGames(staticGames);
+            return;
+        }
+
+        const searchedGames = staticGames.filter(game =>
+            game.name.toLowerCase().includes(searchQuery)
+        );
+
+        setGames(searchedGames);
+    }
+
+    const searchCallback = useCallback(debounce(search, 500), [searchQuery]);
+
+    useEffect(() => {
+        setSearching(true);
+        searchCallback();
+
+        return searchCallback.cancel;
+    }, [searchQuery, searchCallback]);
+
+    console.log(isSearching);
+
     return (
         <Wrapper>
             <Container>
                 <h1>Qual jogo você quer jogar?</h1>
                 <TextInput
                     placeholder='Pesquise um jogo...'
-                    icon={<MdSearch size={20} />}
+                    onChange={e => setSearchQuery(e.target.value.toLowerCase())}
+                    icon={
+                        isSearching ? (
+                            <CircleSpinner size={20} color='#626770' />
+                        ) : (
+                            <MdSearch size={20} />
+                        )
+                    }
                 />
                 <GameList>
                     {games.map(game => (
-                        <GameBanner key={game.name}>
+                        <GameBanner
+                            onClick={() => handleClick(game.name)}
+                            key={game.name}>
                             <img src={game.imageUrl} />
                         </GameBanner>
                     ))}
