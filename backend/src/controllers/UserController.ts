5defdf24884b599eefd0314ca1f5237392739596
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-15 19:39:54 -0300

    Criaçao controller reset de senha

--- backend/src/controllers/UserController.ts
+++ backend/src/controllers/UserController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import User from '../models/User';
 import * as Yup from 'yup';
 import bcrypt from 'bcryptjs';
+import { v4 as uuid } from 'uuid';
 
 class UserController {
     public async index(req: Request, res: Response): Promise<Response> {
@@ -50,8 +51,13 @@ class UserController {
         }
     }
 
-    public async changePassword(req: Request, res: Response) {
-        const user = await User.findById(res.locals['user'].id).select('+password');
+    public async changePassword(
+        req: Request,
+        res: Response
+    ): Promise<Response> {
+        const user = await User.findById(res.locals['user'].id).select(
+            '+password'
+        );
 
         const schema = Yup.object().shape({
             password: Yup.string().required('A senha deve ser digitada'),
@@ -73,22 +79,61 @@ class UserController {
 
         try {
             if (!user)
-                return res.status(404).send({ error: 'Usuário não localizado.' });
+                return res
+                    .status(404)
+                    .send({ error: 'Usuário não localizado.' });
 
             if (!bcrypt.compareSync(req.body.password, user.password))
                 return res.status(400).send({ error: 'Senha incorreta.' });
 
             if (req.body.newPassword != req.body.confirmPassword)
-                return res.status(400).send({ error: 'Nova senha e confirmação não coincidem.' });
+                return res
+                    .status(400)
+                    .send({ error: 'Nova senha e confirmação não coincidem.' });
 
             let newPassword = await bcrypt.hash(req.body.newPassword, 10);
             await User.findByIdAndUpdate(user.id, { password: newPassword });
         } catch (err) {
-            console.log(err)
-            return res.status(400).send({ error: 'Falha ao atualizar a senha.' });
+            console.log(err);
+            return res
+                .status(400)
+                .send({ error: 'Falha ao atualizar a senha.' });
         }
 
         res.status(200).send('Senha atualizada com sucesso!');
+    }
+
+    public async forgotPassowrd(
+        req: Request,
+        res: Response
+    ): Promise<Response> {
+        const { email } = req.body;
+
+        const schema = Yup.object().shape({
+            email: Yup.string()
+                .required('Email is required')
+                .email('Email is invalid'),
+        });
+
+        try {
+            await schema.validate(req.body, {
+                abortEarly: false,
+            });
+        } catch (err) {
+            const errors = {};
+            err.inner.forEach(error => {
+                errors[error.path] = error.message;
+            });
+            return res.status(400).send(errors);
+        }
+
+        try {
+            if (await User.findOne({ email })) {
+                return res.status(201).json({ token: uuid() });
+            }
+        } catch (err) {
+            return res.status(400).send({ error: 'E-mail inválido.' });
+        }
     }
 }
 
