8e6174bbf6652fbdcbe46c6364cb4cc03f1f5fc5
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-15 03:22:04 -0300

    Criada página de meus agendamentos

---
+++ frontend/src/pages/MySchedules/index.tsx
@@ -0,0 +1,135 @@
+import React, { useState, useEffect } from 'react';
+import { MdMoreVert } from 'react-icons/md';
+
+import {
+    Container,
+    DayList,
+    DaySchedules,
+    ScheduleList,
+    Schedule,
+    ScheduleTime,
+    ScheduleJoint,
+    ScheduleDetails,
+    ScheduleInfo,
+} from './styles';
+import moment from 'moment';
+import API from '../../services/api';
+
+interface IScheduleStation {
+    _id: string;
+    name: string;
+}
+
+interface IScheduleGame {
+    _id: string;
+    name: string;
+}
+
+interface ISchedule {
+    _id: string;
+    date: string;
+    horary: string;
+    station: IScheduleStation;
+    game: IScheduleGame;
+    time: number;
+}
+
+interface IDay {
+    label: string;
+    date: string;
+    schedules: ISchedule[];
+}
+
+const MySchedules: React.FC = () => {
+    const [currentDays, setCurrentDays] = useState<IDay[]>([]);
+
+    async function loadDays() {
+        const { data: schedules } = await API.get<ISchedule[]>('schedules');
+
+        const firstDate = moment();
+
+        const dayLabels = [
+            'Domingo',
+            'Segunda-feira',
+            'Terça-feira',
+            'Quarta-feira',
+            'Quinta-feira',
+            'Sexta-feira',
+            'Sábado',
+        ];
+
+        firstDate.subtract(moment().day(), 'days');
+
+        const days = [];
+
+        for (let i = 0; i < 7; i++) {
+            const currentDate = moment();
+
+            currentDate.add(i, 'days');
+
+            const day: IDay = {
+                label: dayLabels[i],
+                date: currentDate.format('DD/MM/YY'),
+                schedules: [],
+            };
+
+            schedules.forEach(schedule => {
+                const scheduleDate = moment(schedule.date);
+                if (currentDate.day() === scheduleDate.day()) {
+                    day.schedules.push({
+                        ...schedule,
+                        horary: scheduleDate.format('HH:mm'),
+                    });
+                }
+            });
+
+            days.push(day);
+        }
+
+        setCurrentDays(days);
+    }
+
+    useEffect(() => {
+        loadDays();
+    }, []);
+
+    return (
+        <Container>
+            <DayList>
+                {currentDays.map(day => (
+                    <DaySchedules key={day.date}>
+                        <h2>{day.label}</h2>
+                        <small>Dia {day.date}</small>
+                        {day.schedules.length > 0 ? (
+                            <ScheduleList>
+                                {day.schedules.map(schedule => (
+                                    <Schedule key={schedule._id}>
+                                        <ScheduleTime>
+                                            <span>{schedule.horary}</span>
+                                            <ScheduleJoint />
+                                        </ScheduleTime>
+                                        <ScheduleDetails>
+                                            <ScheduleInfo>
+                                                <span>
+                                                    {schedule.game.name}
+                                                </span>
+                                                <span>
+                                                    {schedule.station.name}
+                                                </span>
+                                            </ScheduleInfo>
+                                            <MdMoreVert size={24} />
+                                        </ScheduleDetails>
+                                    </Schedule>
+                                ))}
+                            </ScheduleList>
+                        ) : (
+                            <p>Você não tem nenhum horário marcado esse dia!</p>
+                        )}
+                    </DaySchedules>
+                ))}
+            </DayList>
+        </Container>
+    );
+};
+
+export default MySchedules;
