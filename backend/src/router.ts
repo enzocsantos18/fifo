480bd37e2237d3c27c286f2a2b8785269d4c09d1
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-15 21:16:49 -0300

    Adicionando rotas de alteraçao e reset de senha, exclusao de usuario e agendamento

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -10,6 +10,9 @@ import Storage from './config/storage';
 const router = Router();
 
 router.post('/users', UserController.create);
+router.post('/users/forgotpassword', UserController.forgotPassowrd);
+router.post('/users/changepassword', AuthMiddleware, UserController.changePassword);
+router.delete('/users', AuthMiddleware, UserController.deleteUser);
 
 router.get('/games', GameController.index);
 router.post('/games', [Storage.single('image')], GameController.create);
@@ -18,8 +21,7 @@ router.get('/games/:id/stations', StationController.indexByGame);
 
 router.get('/schedules', AuthMiddleware, ScheduleController.index);
 router.post('/schedules', AuthMiddleware, ScheduleController.create);
-router.patch('/schedules/:id', ScheduleController.update);
-router.delete('/schedules/:id', ScheduleController.delete);
+router.delete('/schedules/:id', AuthMiddleware, ScheduleController.delete);
 
 router.post('/auth/login', AuthController.login);
 router.post('/auth/logout', AuthController.logout);
