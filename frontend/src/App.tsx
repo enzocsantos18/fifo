658c5f5d6c68dc63de2af97bc3522ac03f400e54
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-17 16:33:01 -0300

    Carregamento dos dados do usuário no início da aplicação

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,37 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Routes from './Routes';
 import GlobalStyle from './styles/GlobalStyle';
-import Nav from './components/Nav/index';
+import Auth from './services/auth';
+import API from './services/api';
+import { IUser, UserContext } from './contexts/User';
 
 function App() {
+    const [userData, setUserData] = useState<IUser | null>(null);
+
+    function loadUserData() {
+        if (Auth.hasToken()) {
+            API.get('users')
+                .then(({ data }) => {
+                    setUserData({
+                        ...data,
+                        firstName: data['name'].split(' ')[0],
+                    });
+                })
+                .catch(() => {
+                    Auth.destroyToken();
+                });
+        }
+    }
+
+    useEffect(() => {
+        loadUserData();
+    }, []);
+
     return (
-        <>
-            <Nav />
+        <UserContext.Provider value={userData}>
             <Routes />
             <GlobalStyle />
-        </>
+        </UserContext.Provider>
     );
 }
 
