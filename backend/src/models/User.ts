deaacc136d5c38e877a3d9372d221c4d55980ce1
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-17 16:38:24 -0300

    Adicionado imageURL no model de usuário

--- backend/src/models/User.ts
+++ backend/src/models/User.ts
@@ -22,6 +22,9 @@ const UserSchema = new mongoose.Schema({
         required: true,
         select: false,
     },
+    imageURL: {
+        type: String,
+    },
 });
 
 UserSchema.pre('save', async function (this: IUser, next) {
