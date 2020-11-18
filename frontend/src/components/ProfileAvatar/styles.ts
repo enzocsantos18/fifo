540fc477bdf873ec4b6d47d4131ed5aacb7178b4
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-18 17:05:42 -0300

    Inicias do nome e sobrenome caso o usuário não tenha foto no avatar

--- frontend/src/components/ProfileAvatar/styles.ts
+++ frontend/src/components/ProfileAvatar/styles.ts
@@ -6,13 +6,20 @@ export const Container = styled.div`
     border-radius: 50%;
     background: var(--primary0);
     border: 3px solid var(--primary2);
+    display: flex;
+    justify-content: center;
+    align-items: center;
 
     & img {
-        display: inline;
-        margin: 0 auto;
         height: 100%;
         width: 100%;
         object-fit: cover;
         border-radius: 50%;
+    }
+
+    & p {
+        font-size: 18px;
+        font-weight: 700;
+        color: #fff;
     }
 `;
