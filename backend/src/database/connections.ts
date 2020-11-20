097034e578149a6cd4ee5db6f707da2eb1d640a3
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-20 15:22:19 -0300

    Configurando o banco de dados para um ambiente de testes

--- backend/src/database/connections.ts
+++ backend/src/database/connections.ts
@@ -1,9 +1,14 @@
 import mongoose from 'mongoose';
 
-mongoose.connect(process.env['DB_URL'], {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-});
+mongoose.connect(
+    process.env.NODE_ENV == 'test'
+        ? 'mongodb://localhost:27017/fifo-test'
+        : process.env['DB_URL'],
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+    }
+);
 
 export default mongoose;
