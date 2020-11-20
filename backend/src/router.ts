f4103d4e16d8f1a653284d78a2d26af582030aa6
JoaooGabriel <caetanojoaogabriel@gmail.com>
2020-11-20 13:24:00 -0300

    Validando rota de cadastro de jogos e estações, somente usuário admin

--- backend/src/router.ts
+++ backend/src/router.ts
@@ -5,6 +5,7 @@ import ScheduleController from "./controllers/ScheduleController";
 import AuthController from "./controllers/AuthController";
 import StationController from "./controllers/StationController";
 import AuthMiddleware from "./middlewares/auth";
+import AdminMiddleware from "./middlewares/admin";
 import { GameBannerStorage, UserAvatarStorage } from "./config/storage";
 
 const router = Router();
@@ -21,15 +22,19 @@ router.post(
   AuthMiddleware,
   UserController.changePassword
 );
-router.delete("/users", AuthMiddleware, UserController.deleteUser);
+router.post("/users/delete", AuthMiddleware, UserController.deleteUser);
 
 router.get("/games", GameController.index);
 router.post(
   "/games",
-  [GameBannerStorage.single("image")],
+  [GameBannerStorage.single("image"), AuthMiddleware, AdminMiddleware],
   GameController.create
 );
-router.post("/games/stations", StationController.create);
+router.post(
+  "/games/stations",
+  [AuthMiddleware, AdminMiddleware],
+  StationController.create
+);
 router.get("/games/:id/stations", StationController.indexByGame);
 
 router.get("/schedules", AuthMiddleware, ScheduleController.index);
