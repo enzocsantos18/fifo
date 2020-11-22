b9a8f0ce9104af8c2cac110b7dbd7065388f0354
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-22 20:46:20 -0300

    Alguns testes de agendamento removidos

--- backend/tests/unit/schedule.spec.ts
+++ backend/tests/unit/schedule.spec.ts
@@ -47,70 +47,14 @@ describe('Agendamento', () => {
         await request(server)
             .post('/schedules')
             .send({
-                date,
                 station: defaultStation._id,
                 game: defaultGame._id,
-                time: 30,
             })
             .set('Authorization', `Bearer ${authToken}`);
 
         const schedules = await Schedule.find({ user: authUser._id });
 
         expect(schedules).to.have.lengthOf(1);
-    });
-
-    it('Usuário autenticado não pode agendar no mesmo horário de outra pessoa', async () => {
-        const schedule = await Schedule.findOne();
-
-        const response = await request(server)
-            .post('/schedules')
-            .send({
-                date: schedule.date,
-                station: defaultStation._id,
-                game: defaultGame._id,
-                time: 30,
-            })
-            .set('Authorization', `Bearer ${authToken}`);
-
-        expect(response.status).to.equal(400);
-    });
-
-    it('Usuário autenticado não pode agendar num horário que irá exceder o horário de outra pessoa', async () => {
-        const schedule = await Schedule.findOne();
-        const scheduleDate = moment(schedule.date);
-
-        scheduleDate.subtract(15, 'minutes');
-
-        const response = await request(server)
-            .post('/schedules')
-            .send({
-                date: scheduleDate,
-                station: defaultStation._id,
-                game: defaultGame._id,
-                time: 30,
-            })
-            .set('Authorization', `Bearer ${authToken}`);
-
-        expect(response.status).to.equal(400);
-    });
-
-    it('Usuário autenticado não pode agendar num horário que já está reservado para outra pessoa', async () => {
-        const schedule = await Schedule.findOne();
-        const scheduleDate = moment(schedule.date);
-
-        scheduleDate.add(15, 'minutes');
-
-        const response = await request(server)
-            .post('/schedules')
-            .send({
-                date: scheduleDate,
-                station: defaultStation._id,
-                game: defaultGame._id,
-                time: 30,
-            })
-            .set('Authorization', `Bearer ${authToken}`);
-
-        expect(response.status).to.equal(400);
     });
 
     it('Usuário autenticado deve ver seus agendamentos', async () => {
