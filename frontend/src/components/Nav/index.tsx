3790c9d7084a019091210f3330f44b72b36bafc0
Leonardo Camargo <leoofficevps@gmail.com>
2020-11-26 01:21:22 -0300

    Adicionado item de administração no dropdown e restringindo apenas para administradores

--- frontend/src/components/Nav/index.tsx
+++ frontend/src/components/Nav/index.tsx
@@ -1,11 +1,5 @@
-import React, { useContext } from 'react';
-import {
-    MdAccountCircle,
-    MdExpandMore,
-    MdFiberManualRecord,
-    MdClose,
-    MdWatchLater,
-} from 'react-icons/md';
+import React, { useState, useContext, useEffect } from 'react';
+import { MdAccountCircle, MdExpandMore, MdClose, MdWatchLater, MdBuild } from 'react-icons/md';
 import { UserContext } from '../../contexts/User';
 import Auth from '../../services/auth';
 import { media } from '../../services/media';
@@ -13,16 +7,11 @@ import Dropdown from '../Dropdown';
 import ProfileAvatar from '../ProfileAvatar';
 import { CircleShimmer, LineShimmer } from '../Shimmer';
 
-import {
-    Content,
-    Container,
-    LinkList,
-    LinkItem,
-    ProfileContainer,
-    ProfileContainerShimmerContainer,
-} from './styles';
+import { IItem } from './../Dropdown/';
+import { Content, Container, LinkList, ProfileContainer, ProfileContainerShimmerContainer } from './styles';
 
 const Nav: React.FC = () => {
+    const [dropdownItems, setDropdownItems] = useState<IItem[]>([]);
     const userData = useContext(UserContext);
 
     function handleLogout() {
@@ -30,39 +19,47 @@ const Nav: React.FC = () => {
         window.location.reload();
     }
 
+    useEffect(() => {
+        const items = [
+            {
+                text: 'Fila',
+                route: '/',
+                icon: <MdWatchLater size={20} />,
+            },
+            {
+                text: 'Minha conta',
+                route: '/account/settings',
+                icon: <MdAccountCircle size={20} />,
+            },
+            {
+                text: 'Sair',
+                icon: <MdClose size={20} />,
+                onClick: handleLogout,
+            },
+        ] as IItem[];
+
+        if (userData?.isAdmin) {
+            items.splice(2, 0, {
+                text: 'Administração',
+                route: '/admin',
+                icon: <MdBuild size={20} />,
+            });
+        }
+
+        setDropdownItems(items);
+    }, [userData]);
+
     return (
         <Container>
             <Content>
                 <LinkList></LinkList>
-                <Dropdown
-                    top='70px'
-                    width='180px'
-                    items={[
-                        {
-                            text: 'Fila',
-                            route: '/',
-                            icon: <MdWatchLater size={20} />,
-                        },
-                        {
-                            text: 'Minha conta',
-                            route: '/account/settings',
-                            icon: <MdAccountCircle size={20} />,
-                        },
-                        {
-                            text: 'Sair',
-                            icon: <MdClose size={20} />,
-                            onClick: handleLogout,
-                        },
-                    ]}>
+                <Dropdown top='70px' width='180px' items={dropdownItems}>
                     {userData ? (
                         <ProfileContainer>
                             <span>{userData.shortName}</span>
                             <div>
                                 <ProfileAvatar
-                                    imageURL={
-                                        userData.imageURL &&
-                                        media('user', userData.imageURL, true)
-                                    }
+                                    imageURL={userData.imageURL && media('user', userData.imageURL, true)}
                                     username={userData.name}
                                     showBorder={true}
                                 />
